cfd5ad4dfe380658f18f046c58622b2ceee22d60
zhangyi <1090318353@qq.com>
2019-01-09 19:45:18 +0800

    init
    init

--- data/src/main/java/com/mayezi/data/DataApplication.java
+++ data/src/main/java/com/mayezi/data/DataApplication.java
@@ -3,6 +3,9 @@ package com.mayezi.data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author z
+ */
 @SpringBootApplication
 public class DataApplication {
 
